b4fec1b3a518c7758a1fdcc9d6c7919246795cb3
MD-Moinuddin <moinuddinmd956@gmail.com>
2019-05-13 15:47:53 +0600

    skill nav updated

--- js/script.js
+++ js/script.js
@@ -22,13 +22,13 @@ $(document).ready(function() {
 	            items:1
 	        },
 	        480:{
-	            items:2
+	            items:1
 	        },
 	        768:{
-	            items:3
+	            items:2
 	        },
 	        938:{
-	            items:4
+	            items:3
 	        }
 	    }
 	});
